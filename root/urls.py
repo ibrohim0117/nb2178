5b8fb6b09c9a564bf8b01303d0cfaf3ad1823b9e
ibrohim <ibrohim.dev.uz@gmail.com>
2025-11-05 14:26:05 +0500

    falcon start

---
+++ root/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+
+from app.views import ProductView
+
+from root import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', ProductView.as_view(), name='product_list')
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
