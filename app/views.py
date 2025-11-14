8b457ae13fb00d1c5d9e86d12bbddfa1fef785e0
ibrohim <ibrohim.dev.uz@gmail.com>
2025-11-14 14:29:18 +0500

    14.11.2025

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Product
 
 class ProductView(ListView):
@@ -13,3 +15,22 @@ class ProductDetail(DetailView):
     model = Product
     context_object_name = 'product'
     template_name = 'product/product-details.html'
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'user/profile.html'
+    login_url = 'login'
+
+
+class SettingsView(LoginRequiredMixin, TemplateView):
+    template_name = 'user/settings.html'
+    login_url = 'login'
+
+
+
+class UserLoginView(TemplateView):
+    template_name = 'auth/login.html'
+
+
+class UserRegisterView(TemplateView):
+    template_name = 'auth/register.html'
--- root/urls.py
+++ root/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 
-from app.views import ProductView, ProductDetail
+from app.views import (
+    ProductView, ProductDetail, ProfileView, 
+    SettingsView, UserLoginView, UserRegisterView
+)
 
 from root import settings
 
@@ -11,6 +14,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', ProductView.as_view(), name='product_list'),
     path('product/<int:pk>/', ProductDetail.as_view(), name='product'),
+
+    # users urls
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('settings/', SettingsView.as_view(), name='settings'),
+
+    # auth
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('register/', UserRegisterView.as_view(), name='register'),
 ]
 
 
